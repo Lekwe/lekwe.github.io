1267e80dec8ac7d0858a307a804293f3670a4abc
Lekwe <leokeyn@sina.cn>
2021-05-04 08:59:04 +0800

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,19 @@
+<?php
+    //获取提交参数
+    $a = $_POST["key"];
+    $b = $_POST["key2"];
+
+    echo $_FILES['file']['name'];
+    if (isset($_FILES['file']))
+    {
+        //将文件传到服务器根目录中
+        $Up_Path = $_FILES['file']['name'];
+        $tmpname = $_FILES['file']['tmp_name'];
+        //转移临时文件
+        if(move_uploaded_file($tmpname,$Up_Path)){
+            echo "上传成功";
+        }else{
+            echo "上传失败";
+        }
+    }
+?>
